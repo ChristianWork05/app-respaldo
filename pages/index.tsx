af4dab4064c9af95403d30cd8313407e2f241040
ChristianWork05 <christian.lanza@recargaexpress.com>
2024-10-16 21:29:19 -0400

    primer respaldo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { useEffect, useState } from "react";
+import { fetchApi, queries } from "@/utils/Fetching";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,104 +14,89 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+interface Post {
+  title: string
+  _id: string
+  subTitle:string
+  content:string
+  permaLink:string
+  slug:string
+  seoDescription:string
+  categories: Category[]
+  subCategories: Category[]
+  tags: string
+  imgCarrusel: ImageNew[]
+  imgMiniatura: ImageNew
+  imgTexto: ImageNew[]
+  video: Video
+  authorUid:string
+  authorUsername:string
+  updaterUsername:string
+  status: boolean
+  views: number
+  postFormat:string
+  createdAt: number
+  updatedAt: number
+}
+interface Category {
+  _id: string
+}
+type ImageNew = {
+  _id: string
+  i1024: string
+  i800: string
+  i640: string
+  i320: string
+  videoUrl: string
+  createdAt: number
+  }
+
+  type Video = {
+    _id: string
+    url: string
+    createdAt: number
+    }
+
+interface Results {
+  total: number;
+  results: Post[];
+}
+
 export default function Home() {
+
+  const [data,setData] = useState<Post[]>();
+  useEffect(() => {
+    fetchApi({
+      query: queries.getAllPost,
+      variables: {sort:{createdAt:-1}, skip:0, limit:10, development:"bodasdehoy" },
+    }).then((results:Results) =>{
+      setData(results.results)
+      console.log(results)
+    })
+    
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+  
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          {data?.map((elem, idx) => {
+            return(
+              <div key={idx} >
+              {elem.title}
+              </div>
+            )
+          })
+        }
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
     </div>
   );
 }
